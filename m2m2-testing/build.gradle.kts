6f83d51e260d4da80c8d97afc0e25e1e5b8c881f
Danil Malkin <danilmalkin2015@gmail.com>
2021-07-19 16:34:21 +0300

    Create m2m2-testing module

---
+++ m2m2-testing/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    /* Targets configuration omitted. 
+    *  To find out how to configure the targets, please follow the link:
+    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+    js{
+        browser()
+    }
+    jvm{}
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+    }
+}
