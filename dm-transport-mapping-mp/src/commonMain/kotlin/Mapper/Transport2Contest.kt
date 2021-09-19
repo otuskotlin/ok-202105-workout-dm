a2e49403d1e14bb2dde10e51e98c0ec9e45a1c7f
Danil-Malkin-NN <53613490+Danil-Malkin-NN@users.noreply.github.com>
2021-09-19 22:34:33 +0300

    add read Context

--- dm-transport-mapping-mp/src/commonMain/kotlin/Mapper/Transport2Contest.kt
+++ dm-transport-mapping-mp/src/commonMain/kotlin/Mapper/Transport2Contest.kt
@@ -2,16 +2,24 @@ package Mapper
 
 import InnerModel.ExerciseModel
 import InnerModel.OwnerIdModel
+import InnerModel.WorkoutIdModel
 import InnerModel.WorkoutModel
 import ModelForRequest.CreateWorkout
 import ModelForRequest.ExerciseTransfer
 import ModelForRequest.cruds.CreateWorkoutRequest
+import ModelForRequest.cruds.ReadWorkoutRequest
 import context.MpContext
 
 
 fun MpContext.setQuery(query: CreateWorkoutRequest) = apply {
 	this.onRequest = query.requestId?: ""
 	this.requestWorkout = query.createWorkout?.toModel() ?: WorkoutModel()
+}
+
+fun MpContext.setQuery(query: ReadWorkoutRequest) = apply {
+	this.onRequest = query.requestId?:""
+	this.requestWorkoutId = WorkoutIdModel(query.workoutId?:"")
+
 }
 
 fun CreateWorkout.toModel() = WorkoutModel(
