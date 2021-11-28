d376808f254c4f1236865742614aa0bd0e732f9f
Danil Malkin <danilmalkin2015@gmail.com>
2021-11-28 13:42:40 +0300

    init repo

---
+++ dm-repo-sql/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "ru.ru.otus.kotlin"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val exposedVersion: String by project
+    val postgresDriverVersion: String by project
+//    val testContainersVersion: String by project
+
+    implementation(kotlin("stdlib"))
+
+    implementation(project(":dm-transport-main-mp"))
+
+    implementation("org.postgresql:postgresql:$postgresDriverVersion")
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+}
